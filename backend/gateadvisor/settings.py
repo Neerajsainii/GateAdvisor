5c9d8246cd0072c08114f609e8f7e032a788b538
Neeraj kumar saini <131421945+Neerajsainii@users.noreply.github.com>
2026-04-09 17:59:14 +0530

    Add AllowAny permission to REST framework settings

--- backend/gateadvisor/settings.py
+++ backend/gateadvisor/settings.py
@@ -103,6 +103,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True 
 REST_FRAMEWORK = {
+        "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",  # ← add this
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
